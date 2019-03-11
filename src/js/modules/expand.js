474bf4c48219624421b66460582745ae0a75ce73
sammorrisdesign <mail@sammorrisdesign.co.uk>
2019-03-11 15:35:35 -0400

    expand transition

--- src/js/modules/expand.js
+++ src/js/modules/expand.js
@@ -4,19 +4,14 @@ export default {
     },
 
     bindings: function() {
-        $('.uit-candidates__button').click(function(e) {
+        $('.uit-candidates__button').one('click', function(e) {
             e.preventDefault();
-            this.changeButtonState();
+            this.expandCandidates();
         }.bind(this));
     },
 
-    changeButtonState: function() {
-        const isHidden = $('.uit-candidates__candidates.is-expandable').hasClass('uit-candidates__candidates--hidden');
-
-        if (isHidden) {
-            $('.uit-candidates__candidates.is-expandable').removeClass('uit-candidates__candidates--hidden');
-        } else {
-            $('.uit-candidates__candidates.is-expandable').addClass('uit-candidates__candidates--hidden');
-        }
+    expandCandidates: function() {
+        $('.uit-candidates__candidates.is-expandable').removeClass('uit-candidates__candidates--hidden');
+        $('.uit-candidates__button').addClass('uit-candidates__button--clicked');
     }
 };
